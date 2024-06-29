dcf19374e5848269d08b0d44c519ff598060d7e2
Ruslan D. Revellved <revellved@gmail.com>
2024-06-29 20:50:29 +0500

    (HTB) 🍩 Python script for generate instructions

--- new.py
+++ new.py
@@ -2,6 +2,7 @@
 # Create instruction.md for machine of HTB #
 ######################        ##############
 import os, pathlib
+from re import split
 
 TMP = './helpers/template.md'
 PWD = os.path.dirname(os.path.realpath(__file__))
@@ -11,33 +12,42 @@ details: bool = False
 details_template: list[str] = []
 
 ## Parse template on lines
-def parse_template(line: str):
+def parse_template(line: str, global_template: bool = True):
     global details, details_template
 
-    if line == '<details>':
-        details = True
-        return line + '\n'
-    elif line == '</details>':
-        details = False
-        return DETAILS_MARK + '\n' + line + '\n'
-    elif details == True:
-        details_template.append(line)
-        return ''
+    if global_template:
+        if line.strip() == '</details><br>':
+            details = False
+            details_template.append(line) 
+            return DETAILS_MARK
+        elif line.strip() == '<details>':
+            details = True
+            details_template.append(line) 
+            return ''
+        elif details == True:
+            details_template.append(line)
+            return ''
     
     splits = line.split('$')
     for i in range(1, len(splits), 2):
-        splits[i] = input('Value for ' + splits[i] + ':')
+        splits[i] = input('Value for ' + splits[i] + ': ')
+        if splits[i] == '':
+            return ''
 
-    return '$'.join(splits) + '\n'
+    return ''.join(splits)
 
 def fill_details():
+    print('\n-- Fill Details')
+
     output = ""
-    user_input = 'Y'
+    user_input = ''
     while user_input != 'N':
-        if user_input == 'Y':
+        if user_input == '' or user_input == 'Y':
+            print()
             for line in details_template:
-                output += parse_template(line) + '\n'
-        user_input = input('Are there any other questions? (Y/n):').upper()
+                output += parse_template(line, False)
+            print()
+        user_input = input('Are there any other questions? (Y/n): ').upper()
     
     return output
     
@@ -50,12 +60,13 @@ def generate_instruction():
         template = f.readlines()
     
     for line in template:
-        line = line.replace('\n', '')
         output += parse_template(line)
 
     return output
 
 def save(pathdir, text):
+    print('Save to ' + pathdir)
+
     path = pathlib.Path(PWD + '/' + pathdir)
     path.mkdir(parents=True, exist_ok=True)
     with open(pathdir + 'README.md', 'w') as fp:
@@ -63,8 +74,12 @@ def save(pathdir, text):
 
 ## MAIN
 def main():
+    print('-- Hello, let\'s generating instruction\n')
     instruction = generate_instruction().replace(DETAILS_MARK, fill_details())
-    pathdir = input('Save to (dir):')
+    pathdir = input('\nSave to (dir): ')
+    if not pathdir.endswith('/'):
+        pathdir += "/"
+
     save(pathdir, instruction)
     
 ## RUN
